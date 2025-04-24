5c39876e211eb850b1b99ae867ad64aea4f694e7
Thiago Wyse <contatothiagowyseoficial@gmail.com>
2025-04-23 23:42:16 -0300

    feat: Conexão com o banco de dados

--- backend/src/main/java/com/projeto/util/DataBaseConnection.java
+++ backend/src/main/java/com/projeto/util/DataBaseConnection.java
@@ -1,4 +1,24 @@
 package com.projeto.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/sistema_hospitalar";
+    private static final String USER = "postgres"; // Substitua pelo seu usuário do PostgreSQL
+    private static final String PASSWORD = "123"; // Substitua pela sua senha do PostgreSQL
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexão com o banco de dados PostgreSQL estabelecida com sucesso!");
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
