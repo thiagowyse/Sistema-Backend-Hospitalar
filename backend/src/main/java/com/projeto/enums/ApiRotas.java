86d5e81f62c8877929f679ea8782087daa457ecb
Thiago Wyse <contatothiagowyseoficial@gamil.com>
2025-04-27 22:42:37 -0300

    feat: Criação de rotas v3

--- backend/src/main/java/com/projeto/enums/ApiRotas.java
+++ backend/src/main/java/com/projeto/enums/ApiRotas.java
@@ -3,37 +3,87 @@ package com.projeto.enums;
 
 public enum ApiRotas {
 
+    //Rotas Agendamento
+    AGENDAMENTO("/agendamento"),
+    AGENDAMENTO_CREATE("/agendamento/create"),
+    AGENDAMENTO_FIND_BY_ID("/agendamento/findById"),
+    AGENDAMENTO_FIND_BY_FILTER("/agendamento/findByFilter"),
+    AGENDAMENTO_FIND_ALL("/agendamento/findAll"),
+    AGENDAMENTO_UPDATE("/agendamento/update"),
+    AGENDAMENTO_DELETE("/agendamento/delete"),
 
-    //Rotas Usuario
-    USUARIO("/usuario"),
-    USUARIO_AUTHENTICATOR("/usuario/authenticator"),
-    USUARIO_CREATE("/usuario/create"),
-    USUARIO_FIND_BY_ID("/usuario/findById"),
-    USUARIO_FIND_BY_FILTER("/usuario/findByFilter"),
-    USUARIO_FIND_ALL("/usuario/findAll"),
-    USUARIO_UPDATE("/usuario/update"),
-    USUARIO_DELETE("usuario/delete"),
+    //Rotas Atestado
+    ATESTADO("/atestado"),
+    ATESTADO_CREATE("/atestado/create"),
+    ATESTADO_FIND_BY_ID("/atestado/findById"),
+    ATESTADO_FIND_BY_FILTER("/atestado/findByFilter"),
+    ATESTADO_FIND_ALL("/atestado/findAll"),
+    ATESTADO_UPDATE("/atestado/update"),
+    ATESTADO_DELETE("/atestado/delete"),
+
+    //Rotas Declaracao
+    DECLARACAO("/declaracao"),
+    DECLARACAO_CREATE("/declaracao/create"),
+    DECLARACAO_FIND_BY_ID("/declaracao/findById"),
+    DECLARACAO_FIND_BY_FILTER("/declaracao/findByFilter"),
+    DECLARACAO_FIND_ALL("/declaracao/findAll"),
+    DECLARACAO_UPDATE("/declaracao/update"),
+    DECLARACAO_DELETE("/declaracao/delete"),
+
+    //Rotas Enfermeiro
+    ENFERMEIRO("/enfermeiro"),
+    ENFERMEIRO_CREATE("/enfermeiro/create"),
+    ENFERMEIRO_FIND_BY_ID("/enfermeiro/findById"),
+    ENFERMEIRO_FIND_BY_FILTER("/enfermeiro/findByFilter"),
+    ENFERMEIRO_FIND_ALL("/enfermeiro/findAll"),
+    ENFERMEIRO_UPDATE("/enfermeiro/update"),
+    ENFERMEIRO_DELETE("/enfermeiro/delete"),
+
+    //Rotas Especialidade
+    ESPECIALIDADE("/especialidade"),
+    ESPECIALIDADE_CREATE("/especialidade/create"),
+    ESPECIALIDADE_FIND_BY_ID("/especialidade/findById"),
+    ESPECIALIDADE_FIND_BY_FILTER("/especialidade/findByFilter"),
+    ESPECIALIDADE_FIND_ALL("/especialidade/findAll"),
+    ESPECIALIDADE_UPDATE("/especialidade/update"),
+    ESPECIALIDADE_DELETE("/especialidade/delete"),
+
+    //Rotas Exame
+    EXAME("/exame"),
+    EXAME_CREATE("/exame/create"),
+    EXAME_FIND_BY_ID("/exame/findById"),
+    EXAME_FIND_BY_FILTER("/exame/findByFilter"),
+    EXAME_FIND_ALL("/exame/findAll"),
+    EXAME_UPDATE("/exame/update"),
+    EXAME_DELETE("/exame/delete"),
 
 
-    //Rotas Paciente
-    PACIENTE("/paciente"),
-    PACIENTE_CREATE("/paciente/create"),
-    PACIENTE_FIND_BY_ID("/paciente/findById"),
-    PACIENTE_FIND_BY_FILTER("/paciente/findByFilter"),
-    PACIENTE_FIND_ALL("/paciente/findAll"),
-    PACIENTE_UPDATE("/paciente/update"),
-    PACIENTE_DELETE("/paciente/delete"),
+    //Rotas Historico Atendimento
+    HISTORICO_ATENDIMENTO("/historicoAtendimento"),
+    HISTORICO_ATENDIMENTO_CREATE("/historicoAtendimento/create"),
+    HISTORICO_ATENDIMENTO_FIND_BY_ID("/historicoAtendimento/findById"),
+    HISTORICO_ATENDIMENTO_FIND_BY_FILTER("/historicoAtendimento/findByFilter"),
+    HISTORICO_ATENDIMENTO_FIND_ALL("/historicoAtendimento/findAll"),
+    HISTORICO_ATENDIMENTO_UPDATE("/historicoAtendimento/update"),
+    HISTORICO_ATENDIMENTO_DELETE("/historicoAtendimento/delete"),
 
+    //Rotas LogUsuario
+    LOG_USUARIO("/logUsuario"),
+    LOG_USUARIO_CREATE("/logUsuario/create"),
+    LOG_USUARIO_FIND_BY_ID("/logUsuario/findById"),
+    LOG_USUARIO_FIND_BY_FILTER("/logUsuario/findByFilter"),
+    LOG_USUARIO_FIND_ALL("/logUsuario/findAll"),
+    LOG_USUARIO_UPDATE("/logUsuario/update"),
+    LOG_USUARIO_DELETE("/logUsuario/delete"),
 
-    //Rotas Recepcionista
-    RECEPCIONISTA("/recepcionista"),
-    RECEPCIONISTA_CREATE("/recepcionista/create"),
-    RECEPCIONISTA_FIND_ALL("/recepcionista/findAll"),
-    RECEPCIONISTA_FIND_BY_ID("/recepcionista/findById"),
-    RECEPCIONISTA_FIND_BY_FILTER("/recepcionista/findByFilter"),
-    RECEPCIONISTA_UPDATE("/recepcionista/update"),
-    RECEPCIONISTA_DELETE("/recepcionista/delete"),
-
+    //Rotas Medicamento
+    MEDICAMENTO("/medicamento"),
+    MEDICAMENTO_CREATE("/medicamento/create"),
+    MECDICAMENTO_FIND_BY_ID("/medicamento/findById"),
+    MEDICAMENTO_FIND_BY_FILTER("/medicamento/findByFilter"),
+    MEDICAMENTO_FIND_ALL("/medicamento/findAll"),
+    MEDICAMENTO_UPDATE("/medicamento/update"),
+    MEDICAMENTO_DELETE("/medicamento/delete"),
 
     //Rotas Medico
     MEDICO("/medico"),
@@ -44,15 +94,22 @@ public enum ApiRotas {
     MEDICO_UPDATE("/medico/update"),
     MEDICO_DELETE("/medico/delete"),
 
-    //Rotas Prontuario
-    PRONTUARIO("/prontuario"),
-    PRONTUARIO_CREATE("/prontuario/create"),
-    PRONTUARIO_FIND_BY_ID("/prontuario/findById"),
-    PRONTUARIO_FIND_BY_FILTER("/prontuario/findByFilter"),
-    PRONTUARIO_FIND_ALL("/prontuario/findAll"),
-    PRONTUARIO_UPDATE("/prontuario/update"),
-    PRONTUARIO_DELETE("/prontuario/delete"),
+    MEDICO_ESPECIALISTA("/medicoEspcialista"),
+    MEDICO_ESPECIALISTA_CREATE("/medicoEspecialista/create"),
+    MEDICO_ESPECIALISTA_FIND_BY_ID("/medicoEspecialista/findById"),
+    MEDICO_ESPECIALISTA_FIND_BY_FILTER("/medicoEspecialista/findByFilter"),
+    MEDICO_ESPECIALISTA_FIND_ALL("/medicoEspecialista/findAll"),
+    MEDICO_ESPECIALISTA_UPDATE("/medicoEspecialista/update"),
+    MEDICO_ESPECIALISTA_DELETE("/medicoEspecialista/delete"),
 
+    //Rotas Paciente
+    PACIENTE("/paciente"),
+    PACIENTE_CREATE("/paciente/create"),
+    PACIENTE_FIND_BY_ID("/paciente/findById"),
+    PACIENTE_FIND_BY_FILTER("/paciente/findByFilter"),
+    PACIENTE_FIND_ALL("/paciente/findAll"),
+    PACIENTE_UPDATE("/paciente/update"),
+    PACIENTE_DELETE("/paciente/delete"),
 
     //Rotas Perfil
     PERFIL("/perfil"),
@@ -64,44 +121,34 @@ public enum ApiRotas {
     PERFIL_DELETE("/perfil/delete"),
 
 
-    //Rotas Atendimento
-    ATENDIMENTO("/atendimento"),
-    ATENDIMENTO_CREATE("/atendimento/create"),
-    ATENDIMENTO_FIND_BY_ID("/atendimento/findById"),
-    ATENDIMENTO_FIND_BY_FILTER("/atendimento/findByFilter"),
-    ATENDIMENTO_FIND_ALL("/atendimento/findAll"),
-    ATENDIMENTO_UPDATE("/atendimento/update"),
-    ATENDIMENTO_DELETE("/atendimento/delete"),
+    //Rotas Prontuario
+    PRONTUARIO("/prontuario"),
+    PRONTUARIO_CREATE("/prontuario/create"),
+    PRONTUARIO_FIND_BY_ID("/prontuario/findById"),
+    PRONTUARIO_FIND_BY_FILTER("/prontuario/findByFilter"),
+    PRONTUARIO_FIND_ALL("/prontuario/findAll"),
+    PRONTUARIO_UPDATE("/prontuario/update"),
+    PRONTUARIO_DELETE("/prontuario/delete"),
 
 
-    //Rotas LogUsuario
-    LOG_USUARIO("/logUsuario"),
-    LOG_USUARIO_CREATE("/logUsuario/create"),
-    LOG_USUARIO_FIND_BY_ID("/logUsuario/findById"),
-    LOG_USUARIO_FIND_BY_FILTER("/logUsuario/findByFilter"),
-    LOG_USUARIO_FIND_ALL("/logUsuario/findAll"),
-    LOG_USUARIO_UPDATE("/logUsuario/update"),
-    LOG_USUARIO_DELETE("/logUsuario/delete"),
+    //Rotas Prontuario Exame
+    PRONTUARIO_EXAME("/prontuarioExame"),
+    PRONTUARIO_EXAME_CREATE("/prontuarioExame/create"),
+    PRONTUARIO_EXAME_FIND_BY_ID("/prontuarioExame/findById"),
+    PRONTUARIO_EXAME_FIND_BY_FILTER("/prontuarioExame/findByFilter"),
+    PRONTUARIO_EXAME_FIND_ALL("/prontuarioExame/findAll"),
+    PRONTUARIO_EXAME_UPDATE("/prontuarioExame/update"),
+    PRONTUARIO_EXAME_DELETE("/prontuarioExame/delete"),
 
 
-    //Rotas Declaracao
-    DECLARACAO("/declaracao"),
-    DECLARACAO_CREATE("/declaracao/create"),
-    DECLARACAO_FIND_BY_ID("/declaracao/findById"),
-    DECLARACAO_FIND_BY_FILTER("/declaracao/findByFilter"),
-    DECLARACAO_FIND_ALL("/declaracao/findAll"),
-    DECLARACAO_UPDATE("/declaracao/update"),
-    DECLARACAO_DELETE("/declaracao/delete"),
-
-
-    //Rotas Exame
-    EXAME("/exame"),
-    EXAME_CREATE("/exame/create"),
-    EXAME_FIND_BY_ID("/exame/findById"),
-    EXAME_FIND_BY_FILTER("/exame/findByFilter"),
-    EXAME_FIND_ALL("/exame/findAll"),
-    EXAME_UPDATE("/exame/update"),
-    EXAME_DELETE("/exame/delete"),
+    //Rotas Prontuario Medicamento
+    PRONTUARIO_MEDICAMENTO("/prontuarioMedicamento"),
+    PRONTUARIO_MEDICAMENTO_CREATE("/prontuarioMedicamneto/create"),
+    PRONTUARIO_MEDICAMENTO_FIND_BY_ID("/prontuarioMedicamento/findById"),
+    PRONTUARIO_MEDICAMENTO_FIND_BY_FILTER("/prontuarioMedicamento/findByFilter"),
+    PRONTUARIO_MEDICAMENTO_FIND_ALL("/prontuarioMedicamento/findAll"),
+    PRONTUARIO_MEDICAMENTO_UPDATE("/prontuarioMedicamento/update"),
+    PRONTUARIO_MEDICAMENTO_DELETE("/prontuarioMedicamento/delete"),
 
 
     //Rotas Receita
@@ -123,35 +170,25 @@ public enum ApiRotas {
     RECEITA_MEDICAMENTO_DELETE("/receitaMedicamento/delete"),
 
 
-    //Rotas Especialidade
-    ESPECIALIDADE("/especialidade"),
-    ESPECIALIDADE_CREATE("/especialidade/create"),
-    ESPECIALIDADE_FIND_BY_ID("/especialidade/findById"),
-    ESPECIALIDADE_FIND_BY_FILTER("/especialidade/findByFilter"),
-    ESPECIALIDADE_FIND_ALL("/especialidade/findAll"),
-    ESPECIALIDADE_UPDATE("/especialidade/update"),
-    ESPECIALIDADE_DELETE("/especialidade/delete"),
+    //Rotas Recepcionista
+    RECEPCIONISTA("/recepcionista"),
+    RECEPCIONISTA_CREATE("/recepcionista/create"),
+    RECEPCIONISTA_FIND_ALL("/recepcionista/findAll"),
+    RECEPCIONISTA_FIND_BY_ID("/recepcionista/findById"),
+    RECEPCIONISTA_FIND_BY_FILTER("/recepcionista/findByFilter"),
+    RECEPCIONISTA_UPDATE("/recepcionista/update"),
+    RECEPCIONISTA_DELETE("/recepcionista/delete"),
 
 
-    //Rotas Enfermeiro
-    ENFERMEIRO("/enfermeiro"),
-    ENFERMEIRO_CREATE("/enfermeiro/create"),
-    ENFERMEIRO_FIND_BY_ID("/enfermeiro/findById"),
-    ENFERMEIRO_FIND_BY_FILTER("/enfermeiro/findByFilter"),
-    ENFERMEIRO_FIND_ALL("/enfermeiro/findAll"),
-    ENFERMEIRO_UPDATE("/enfermeiro/update"),
-    ENFERMEIRO_DELETE("/enfermeiro/delete"),
-
-    // Rotas Historico Atendimento
-    HISTORICO_ATENDIMENTO("/historicoAtendimento"),
-    HISTORICO_ATENDIMENTO_CREATE("/historicoAtendimento/create"),
-    HISTORICO_ATENDIMENTO_FIND_BY_ID("/historicoAtendimento/findById"),
-    HISTORICO_ATENDIMENTO_FIND_BY_FILTER("/historicoAtendimento/findByFilter"),
-    HISTORICO_ATENDIMENTO_FIND_ALL("/historicoAtendimento/findAll"),
-    HISTORICO_ATENDIMENTO_UPDATE("/historicoAtendimento/update"),
-    HISTORICO_ATENDIMENTO_DELETE("/historicoAtendimento/delete");
-
-
+    //Rotas Usuario
+    USUARIO("/usuario"),
+    USUARIO_AUTHENTICATOR("/usuario/authenticator"),
+    USUARIO_CREATE("/usuario/create"),
+    USUARIO_FIND_BY_ID("/usuario/findById"),
+    USUARIO_FIND_BY_FILTER("/usuario/findByFilter"),
+    USUARIO_FIND_ALL("/usuario/findAll"),
+    USUARIO_UPDATE("/usuario/update"),
+    USUARIO_DELETE("usuario/delete");
 
 
     private final String path;
